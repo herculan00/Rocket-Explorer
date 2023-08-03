0b39ce984213721d8328004dc75d459c779b9a72
herculan00 <94049537+herculan00@users.noreply.github.com>
2023-08-03 15:31:38 +0000

    learn JS

---
+++ ROCKET/Aprender JS/Aula07/js.js
@@ -0,0 +1,42 @@
+
+let openMsg = () => {
+  return  prompt(
+        `
+         Olá usuário! Digite o núnemero daopão desejada
+        
+            1 - Cadastrar um item na lista
+            2 - Mostrar itens cadastrados
+            3 - Sair do programa 
+        ` )
+}
+
+let items = []
+
+option = Number(openMsg())
+
+while(option != 3){
+
+    switch (option) {
+        case 1:
+            let item = prompt("Insira o nome do item!")
+            items.push(item)
+        break;
+        
+        case 2:
+            if (items.length == 0) {
+                alert("Não exitem items cadastrados")
+            } else {
+                alert(items)
+            }
+        break;
+
+        default:
+        break;
+    }
+
+    option = Number(openMsg())
+}
+
+alert("Programa terminou")
+
+
