f7618ba549bde60550dab6181f073cd75fd182cf
herculan00 <94049537+herculan00@users.noreply.github.com>
2023-08-31 14:05:36 +0000

    leran JS

--- ROCKET/Aprender JS/Aula09/js.js
+++ ROCKET/Aprender JS/Aula09/js.js
@@ -1,41 +1,32 @@
 
-let openMsg = () => {
-  return  prompt(
-        `
-         Olá usuário! Digite o núnemero daopão desejada
-        
-            1 - Cadastrar um item na lista
-            2 - Mostrar itens cadastrados
-            3 - Sair do programa 
-        ` )
+
+alert("Lista de pacientes")
+
+let patients = [
+    {
+        name:"Pedro",
+        age:20,
+        weight:80,
+        height:1.70,
+    },
+    {
+        name:"Carlos",
+        age:24,
+        weight:85,
+        height:1.76,
+    },
+    {
+        name:"Ze",
+        age:22,
+        weight:89,
+        height:1.78,
+    },
+]
+
+let patientsNames = []
+
+for (const patient of patients) {
+    patientsNames.push(patient.name)
 }
 
-let items = []
-
-option = Number(openMsg())
-
-while(option != 3){
-
-    switch (option) {
-        case 1:
-            let item = prompt("Insira o nome do item!")
-            items.push(item)
-        break;
-        
-        case 2:
-            if (items.length == 0) {
-                alert("Não exitem items cadastrados")
-            } else {
-                alert(items)
-            }
-        break;
-
-        default:
-        break;
-    }
-    option = Number(openMsg())
-}
-
-alert("Programa terminou")
-
-
+alert(patientsNames)
