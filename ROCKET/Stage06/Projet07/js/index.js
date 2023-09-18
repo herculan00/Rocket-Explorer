b1369f6059a9db8035906c8131cca6e829e1bb50
herculan00 <94049537+herculan00@users.noreply.github.com>
2023-09-18 17:41:59 +0000

    js

--- ROCKET/Stage06/Projet07/js/index.js
+++ ROCKET/Stage06/Projet07/js/index.js
@@ -6,26 +6,9 @@ const routes = {
     404:"/ROCKET/Stage06/Projet07/pages/404.html",
 }
 
-function route(event) {
-    event = event || window.event
-    event.preventDefault()
 
-    window.history.pushState({}, "", event.target.href)
 
-    handle()
-}
 
-function handle() {
-    const { pathname }  = window.location
-    const route = routes[pathname] || routes[404]
-
-    fetch(route).then((data)=>{
-      return  data.text()
-    }).then((html)=>{
-        document.querySelector("#app").innerHTML = html
-        console.log(html)
-    })
-}
 
 handle()
 
