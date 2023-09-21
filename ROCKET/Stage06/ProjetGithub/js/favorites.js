92c924b6b86b4ac7a9696ed6df325f661d77f9b6
herculan00 <94049537+herculan00@users.noreply.github.com>
2023-09-21 16:49:45 +0000

    js

--- ROCKET/Stage06/ProjetGithub/js/favorites.js
+++ ROCKET/Stage06/ProjetGithub/js/favorites.js
@@ -1,3 +1,18 @@
+export class GithubUser{
+    static search(username){
+        const endpoint = `https://api.github.com/users/${username}`
+
+        return fetch(endpoint).then(data => data.json())
+                              .then(data => ({
+                                login: data.login,
+                                name: data.name,
+                                public_repos: data.public_repos,
+                                followers: data.followers
+                              }))
+    }
+}
+
+
 export class Favorietes{
     constructor(root){
         this.root = document.querySelector(root)
