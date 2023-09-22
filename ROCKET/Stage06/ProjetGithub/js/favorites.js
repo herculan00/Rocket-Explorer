153fe31a3325d20a2d165316f45babcfd02b3a0c
herculan00 <94049537+herculan00@users.noreply.github.com>
2023-09-22 14:28:25 +0000

    js

--- ROCKET/Stage06/ProjetGithub/js/favorites.js
+++ ROCKET/Stage06/ProjetGithub/js/favorites.js
@@ -13,7 +13,7 @@ export class GithubUser{
 }
 
 
-export class Favorietes{
+export class Favorites{
     constructor(root){
         this.root = document.querySelector(root)
         this.load()
@@ -24,23 +24,62 @@ export class Favorietes{
         
     }
 
+    save(){
+        localStorage.setItem("@github-favorites:", JSON.stringify(this.entries))
+    }
+
+    async add(username){
+
+        try {
+            const user = await GithubUser.search(username)
+
+            if (user.login === undefined) {
+                throw new Error("Usuário não encontrado")
+            }
+
+            this.entries = [user, ...this.entries]
+
+            this.update()
+            this.save()
+
+        } catch (error) {
+            alert(error.message)
+        }
+       
+
+    }
+
     delete(user){
         const filteredEntries = this.entries.filter( entry => {
             return entry.login !== user.login
         })
 
         this.entries = filteredEntries
+        this.update()
+        this.save()
     }
 }
 
 
-export class FavoriesView extends Favorietes{
+export class FavoriesView extends Favorites{
     constructor(root){
         super(root)
 
         this.tbody= this.root.querySelector('table tbody')
 
         this.update()
+
+        this.onadd()
+    }
+
+    onadd(){
+        const addButton = this.root.querySelector('.search button')
+
+        addButton.onclick = () => {
+            const {value} = this.root.querySelector('.search input')
+
+            this.add(value)
+        }
     }
 
     update(){
