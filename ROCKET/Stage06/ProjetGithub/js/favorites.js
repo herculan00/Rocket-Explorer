7f63057f050f195cf73d9e755a98b6f7eca7102f
herculan00 <94049537+herculan00@users.noreply.github.com>
2023-09-22 15:28:59 +0000

    js

--- ROCKET/Stage06/ProjetGithub/js/favorites.js
+++ ROCKET/Stage06/ProjetGithub/js/favorites.js
@@ -1,17 +1,4 @@
-export class GithubUser{
-    static search(username){
-        const endpoint = `https://api.github.com/users/${username}`
-
-        return fetch(endpoint).then(data => data.json())
-                              .then(data => ({
-                                login: data.login,
-                                name: data.name,
-                                public_repos: data.public_repos,
-                                followers: data.followers
-                              }))
-    }
-}
-
+import { GithubUser } from "./githubUser.js"
 
 export class Favorites{
     constructor(root){
@@ -30,7 +17,14 @@ export class Favorites{
 
     async add(username){
 
+       
         try {
+            const userExist = this.entries.find(entry => entry.login === username)
+
+            if (userExist) {
+                throw Error("Usuário já cadastrado")
+            }
+
             const user = await GithubUser.search(username)
 
             if (user.login === undefined) {
@@ -92,6 +86,7 @@ export class FavoriesView extends Favorites{
             https://github.com/${user.login}.png`
 
             row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
             row.querySelector('.user p').textContent = user.name
             row.querySelector('.user span').textContent = user.login
             row.querySelector('.repositories').textContent = user.public_repos
