e3ed6a4803693cf17d19c1d96107c126bc2fe5dc
herculan00 <94049537+herculan00@users.noreply.github.com>
2023-09-24 16:32:13 +0000

    react

--- ROCKET/Stage09/rocketNotes/src/pages/Details/index.jsx
+++ ROCKET/Stage09/rocketNotes/src/pages/Details/index.jsx
@@ -1,9 +1,10 @@
+import { Container } from "./styles"
+
 export function Details(){
   return(
-    <>
+    <Container>
       <h1>Hello world!</h1>
       <p>Rodrigo Leão</p>
-    </>
-    
+    </Container>
   )
 }
