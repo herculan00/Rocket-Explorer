a025fc98e3d6a919df3fb7d4578e88c410d5d780
herculan00 <https://github.com/herculan00>
2023-09-27 17:58:32 +0100

    react

--- ROCKET/Stage09/rocketNotes/src/components/Header/styles.js
+++ ROCKET/Stage09/rocketNotes/src/components/Header/styles.js
@@ -49,6 +49,15 @@ export const Profile = styled.div`
 `
 
 export const Logout= styled.button`
-    background-color: azure;
+    
+
+    border: none;
+    background: none;
+   
+
+    >svg{
+        font-size: 36px;
+        color: ${({theme})=> theme.COLORS.GRAY_100};
+    }
 
 `
