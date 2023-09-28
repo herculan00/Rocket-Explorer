e49e9819b88875a66f9801ae95abd590e5d2ba23
herculan00 <https://github.com/herculan00>
2023-09-28 19:59:35 +0100

    react

---
+++ ROCKET/Stage09/rocketNotes/src/components/ButtonText/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styled"
+
+
+export function ButtonText({title, ...rest}){
+    return(
+        <Container type="button" {...rest}>
+            {title}
+        </Container>
+    )
+}
--- ROCKET/Stage09/rocketNotes/src/pages/Details/index.jsx
+++ ROCKET/Stage09/rocketNotes/src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container, Links} from "./styles"
 
 
 import { Header } from "../../components/Header"
+import { ButtonText } from "../../components/ButtonText"
 import { Section } from "../../components/Section"
 import { Tag } from "../../components/Tag"
 import { Button } from "../../components/button"
@@ -9,6 +10,7 @@ export function Details(){
   return(
     <Container>
       <Header/>
+      <ButtonText title="Excluir a nota"/>
       <Section title="Links Úteis">
         <Links>
           <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
