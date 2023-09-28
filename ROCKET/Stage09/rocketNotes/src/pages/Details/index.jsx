d50404a36e18b09fd6157f24b0d7f9fbd5cca691
herculan00 <https://github.com/herculan00>
2023-09-28 16:32:01 +0100

    react

--- ROCKET/Stage09/rocketNotes/src/pages/Details/index.jsx
+++ ROCKET/Stage09/rocketNotes/src/pages/Details/index.jsx
@@ -1,11 +1,18 @@
-import { Container } from "./styles"
+import { Container, Links } from "./styles"
 
 import { Button } from "../../components/button"
 import { Header } from "../../components/Header"
+import { Section } from "../../components/Section"
 export function Details(){
   return(
     <Container>
       <Header/>
+      <Section title="Links Úteis">
+        <Links>
+          <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
+          <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
+        </Links>
+      </Section>
       <Button title="Voltar"/>
     </Container>
   )
