2f6ccbc21d1f72e0613dc7ce478eae6bd5ffc10d
herculan00 <https://github.com/herculan00>
2023-09-29 17:24:19 +0100

    react

--- ROCKET/Stage09/rocketNotes/src/pages/Details/index.jsx
+++ ROCKET/Stage09/rocketNotes/src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links} from "./styles"
+import { Container, Links, Content} from "./styles"
 
 
 import { Header } from "../../components/Header"
@@ -10,18 +10,31 @@ export function Details(){
   return(
     <Container>
       <Header/>
-      <ButtonText title="Excluir a nota"/>
-      <Section title="Links Úteis">
-        <Links>
-          <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
-          <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
-        </Links>
-      </Section>
-      <Section title="Marcadores">
-        <Tag title="express"/>
-        <Tag title="nodejs"/> 
-      </Section>
-      <Button title="Voltar"/>
+
+      <main>
+        <Content>
+          <ButtonText title="Excluir a nota"/>
+
+          <h1>Introdução ao React</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Recusandae animi excepturi ab unde? Culpa doloremque blanditiis 
+            perferendis numquam atque suscipit, ipsam officia eveniet! Ipsam 
+            nemo assumenda, nisi cupiditate amet optio?
+          </p>
+
+          <Section title="Links Úteis">
+            <Links>
+              <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
+              <li><a href="https://www.rocketseat.com.br/">https://www.rocketseat.com.br/</a></li>
+            </Links>
+          </Section>
+          <Section title="Marcadores">
+            <Tag title="express"/>
+            <Tag title="nodejs"/> 
+          </Section>
+          <Button title="Voltar"/>
+        </Content>
+      </main>
     </Container>
   )
 }
