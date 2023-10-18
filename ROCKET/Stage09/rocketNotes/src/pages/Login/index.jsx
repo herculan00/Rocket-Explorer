fac2be62127a185d13a9b662f611936528e4f61e
herculan00 <jogospc94@gmail.com>
2023-10-18 15:15:37 +0100

    react

--- ROCKET/Stage09/rocketNotes/src/pages/Login/index.jsx
+++ ROCKET/Stage09/rocketNotes/src/pages/Login/index.jsx
@@ -1,8 +1,8 @@
-import {Container, Form} from "./styles"
-import {FiArrowLeft,FiLock,FiMail,FiUser} from "react-icons/fi"
+import {Container, Form, Avatar} from "./styles"
+import {FiArrowLeft,FiLock,FiMail,FiUser,FiCamera} from "react-icons/fi"
 import { Input } from "../../components/Input"
 import { Button } from "../../components/button"
-import { Avatar } from "../../components/Avatar"
+
 
 
 export function Login(){
@@ -14,7 +14,13 @@ export function Login(){
                 </a>
             </header>
             <Form>
-                <Avatar/>
+                <Avatar>
+                    <img src="https://github.com/rodrigorgtic.png" alt="foto usuário" />
+                    <label htmlFor="avatar">
+                        <FiCamera/>
+                        <input id="avatar" type="file" />
+                    </label>
+                </Avatar>
                 <Input icon={FiUser} type="text" placeholder="Nome"/>
                 <Input icon={FiMail} type="text" placeholder="E-mail"/>
                 <Input icon={FiLock} type="password" placeholder="Senha atual"/>
--- ROCKET/Stage09/rocketNotes/src/pages/Login/styles.js
+++ ROCKET/Stage09/rocketNotes/src/pages/Login/styles.js
@@ -28,4 +28,51 @@ export const Form = styled.form`
 
     margin: 30px auto 0;
 
+    >div:nth-child(4){
+        margin-top: 24px;
+    }
+
+`
+
+export const Avatar = styled.div`
+
+    position: relative;
+    margin: -124px auto 32px;
+
+    width: 186px;
+    height: 186px;
+
+    >img{
+        border-radius: 50%;
+        width: 186px;
+        height: 186px;
+    }
+    >label{
+        position: absolute;
+        right: 7px;
+        bottom: 7px;
+
+        width: 48px;
+        height: 48px;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        background: ${({theme})=>theme.COLORS.ORANGE};
+        border-radius: 50%; 
+
+        cursor: pointer;
+
+        svg{
+            width: 20px;
+            height: 20px;
+            color: ${({theme})=>theme.COLORS.BACKGROUND_800};
+        }
+
+        input{
+            display: none;
+        }
+    }
+
 `
