80b99ac1d7446247f14b7b4828daf6e8d4479234
herculan00 <jogospc94@gmail.com>
2023-10-25 16:24:15 +0100

    react

--- ROCKET/Stage09/rocketNotes/src/main.jsx
+++ ROCKET/Stage09/rocketNotes/src/main.jsx
@@ -6,14 +6,14 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 
-import  { Login } from './pages/Login/index.jsx'
+import  { New } from './pages/New/index.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Login />
+      <New/>
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ ROCKET/Stage09/rocketNotes/src/pages/New/index.jsx
@@ -0,0 +1,32 @@
+import { Container, Form} from "./styles"
+import { Header } from "../../components/Header"
+import { ButtonText } from "../../components/ButtonText"
+import { Input } from "../../components/Input"
+import { Section } from "../../components/Section"
+import { Note } from "../../components/Note"
+import { Tag } from "../../components/Tag"
+import { Textarea } from "../../components/Textarea"
+
+import { FiPlus, FiSearch } from "react-icons/fi"
+
+export function New(){
+    return(
+        <Container>
+            <Header/>
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota</h1>
+                        <a href="/">voltar</a>
+                    </header>
+                    <Input placeholder="Título"/>
+                    <Textarea placeholder="Observações"/>
+                    <Section title="Links úteis">
+                        <Tag title="https://rocketseat.com.br/" />
+                    </Section>
+                </Form>
+            </main>
+
+        </Container>
+    )
+}
