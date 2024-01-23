20d6689f0fa85e54e13f09ecff0f9976bd3ccd7f
herculan00 <jogospc94@gmail.com>
2024-01-23 15:43:50 +0000

    learn js

--- ROCKET/Aprender JS/FocusTimer/src/FocusTimer/actions.js
+++ ROCKET/Aprender JS/FocusTimer/src/FocusTimer/actions.js
@@ -1,9 +1,20 @@
 import state from './state.js'
+import * as timer from './timer.js'
 
 export function toggleRunning(){
     state.isRunning = document.documentElement.classList.toggle('running')
+    timer.countDown()
+}
+
+export function reset(){
+    state.isRunning = false;
+    document.documentElement.classList.remove('running')
 }
 
 export function set(){
     console.log("set");
+}
+
+export function toggleMusic(){
+    state.isMute = document.documentElement.classList.toggle('music-on')
 }
---
+++ ROCKET/Aprender JS/FocusTimer/src/FocusTimer/timer.js
@@ -0,0 +1,20 @@
+import state from "./state.js"
+import * as el from './elements.js'
+
+
+export function countDown(){
+    if (!state.isRunning) {
+        return
+    }
+
+    setTimeout(()=> countDown(), 1000)
+}
+
+
+export function updateDisplay(minutes,seconds){
+    minutes = minutes ?? state.minutes
+    seconds = seconds ?? state.seconds
+
+    el.minutes.textContent = String(minutes).padStart(2,"0")
+    el.seconds.textContent = String(seconds).padStart(2,"0")
+}
