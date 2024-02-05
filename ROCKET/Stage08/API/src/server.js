05385e59440ec1fddb1b5e67ef4efc8512efee8a
herculan00 <jogospc94@gmail.com>
2024-02-05 17:11:50 +0000

    api js

--- ROCKET/Stage08/API/src/server.js
+++ ROCKET/Stage08/API/src/server.js
@@ -2,8 +2,10 @@ const express = require("express")
 
 const app = express()
 
-app.get("/", (request,response)=>{ 
-    response.send("Hello World!")
+app.get("/message/:id/:user", (request,response)=>{ 
+
+    const {id,user} = request.params
+    response.send(`Id da mensagem: ${id} do user: ${user}.`)
 })
 
 const PORT = 3333
