a4d281dd2cc9331d1b1da7454f65b5ea9c9d9109
herculan00 <jogospc94@gmail.com>
2024-02-06 14:37:02 +0000

    api js

--- ROCKET/Stage08/API/src/server.js
+++ ROCKET/Stage08/API/src/server.js
@@ -8,6 +8,12 @@ app.get("/message/:id/:user", (request,response)=>{
     response.send(`Id da mensagem: ${id} do user: ${user}.`)
 })
 
+app.get("/users",(req,res)=>{
+    const {page,limit} = req.query
+
+    res.send(`Pagina: ${page} e limite: ${limit}`)
+})
+
 const PORT = 3333
 
 app.listen(PORT, () => {console.log(`Server is running on Port ${PORT}`)})
